083b5343eba0ead381f0841c092ed0240507266c
Abrikoseg <admin@abrikoseg.ru>
2019-02-14 15:37:59 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+include("Telegram.php");
+include("config.php");
+require_once('service.php');
+
+$telegram = new Telegram(BOT_ID);
+$service = new Service($telegram);
+
+$text = $telegram->Text();
+$chat_id = $telegram->ChatID();
+$message = $telegram->Message();
+
+if($message['new_chat_member']['id']>0) {
+    $telegram->deleteMessage([
+        'chat_id'=>$message['chat']['id'],
+        'message_id'=>$message['message_id']
+    ]);
+}
