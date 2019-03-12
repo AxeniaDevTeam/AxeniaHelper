bb9d4363b71c1d8f281ae67c57a0d238a05c3d12
Abrikoseg <admin@abrikoseg.ru>
2019-03-12 14:54:43 +0500

    delete forwards

--- index.php
+++ index.php
@@ -17,3 +17,11 @@ if($message['new_chat_member']['id']>0) {
         'message_id'=>$message['message_id']
     ]);
 }
+
+
+if($message['forward_from']['id']>0) {
+    $telegram->deleteMessage([
+        'chat_id'=>$message['chat']['id'],
+        'message_id'=>$message['message_id']
+    ]);
+}
