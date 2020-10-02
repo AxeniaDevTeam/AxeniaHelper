c088625a3f5bc4f376f7081f3383b560462f9615
Cyril Fomenko <rik.pie@gmail.com>
2020-10-02 09:24:24 +0500

    Add helping features

--- index.php
+++ index.php
@@ -26,9 +26,26 @@ if($message['forward_from']['id']>0) {
     ]);
 }
 
-if($text=='/test') {
-    $telegram->deleteMessage([
-        'chat_id'=>$message['chat']['id'],
-        'text'=>'text'
-    ]);
+if(in_array($message['from']['id'],SU)){
+    if($text=="/reboot"){
+        $service->rebootAxenia();
+        $telegram->sendMessage(['chat_id'=>$message['from']['id'],'text'=>'Очередь очищена']);
+    }
+}
+
+$axenia=new Telegram(AXENIA_BOT_ID); 
+$webhookInfoResult=$axenia->getWebhookInfo()['result'];
+if($webhookInfoResult['pending_update_count']>1000)
+    foreach(SU as $admin){
+        $telegram->sendMessage([
+            'chat_id'=>$admin,
+            'text'=>"Слишком большая очередь (".$webhookInfoResult['pending_update_count'].")\r\n".
+                    "Последняя ошибка: ".$webhookInfoResult['last_error_message']." ".$webhookInfoResult['last_error_date']."\r\n\r\n".
+                    "Рекомендуется сброс очереди /reboot"
+        ]);
+    }
+
+if($webhookInfoResult['pending_update_count']>1500) {
+    $service->rebootAxenia();
+    $telegram->sendMessage(['chat_id'=>SU[0],'text'=>'Автоматический сброс очереди']);
 }
